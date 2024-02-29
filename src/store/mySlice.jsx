40d496f66c14629de45dc3e4241a1e82f402b085
unknown <fuaad29@gmail.com>
2024-02-29 22:01:57 +0500

    code

--- src/store/mySlice.jsx
+++ src/store/mySlice.jsx
@@ -22,7 +22,7 @@ const productSlice = createSlice({
     "id":1,
     "title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
     "price":109.95,
-    "quantity": 10,
+    "quantity": 21,
     "description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
     "category":"men's clothing",
     "image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
@@ -30,7 +30,6 @@ const productSlice = createSlice({
       "rate":3.9,
       "count":120
     },
-    quantity: 21
   }],
   reducers: {
     add(state, action) {
